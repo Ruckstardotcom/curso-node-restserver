59f4138e8d0d27473addf1bbd091e82492508a2a
ruben.lezcano <ruben.lezcano@flockit.com.ar>
2021-12-07 09:43:17 -0300

    README.md añadido

--- app.js
+++ app.js
@@ -9,3 +9,7 @@ const servidor = new Server();
 
 servidor.listen();
 
+
+
+//Nota: si borro accidentalmente alguna carpeta o quiero volver a la version basica del cascaron vacio
+//usar el siguiente comando de git: git checkout -- .
